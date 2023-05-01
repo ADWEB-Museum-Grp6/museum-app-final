f22458269ec3db5f8c55989ac92d66e717f64910
Sam Buenaventura <spbuenaventura1@student.hau.edu.ph>
2023-05-01 23:03:18 +0800

    yes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { FestivalComponent } from './festival/festival.component';
+import { ReligionComponent } from './religion/religion.component';
+import { CuisineComponent } from './cuisine/cuisine.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    AboutUsComponent,
+    HeroesComponent,
+    FestivalComponent,
+    ReligionComponent,
+    CuisineComponent,
+    FooterComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
